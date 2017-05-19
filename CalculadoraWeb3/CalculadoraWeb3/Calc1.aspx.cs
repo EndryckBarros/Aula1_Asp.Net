c3eab4a4e12648e159af46bf9a1306a6ec706138
1610832 <1610832@045LAB04005>
2017-05-18 22:27:16 -0300

    Session - View State Aula 18/05 Calculos

---
+++ CalculadoraWeb3/CalculadoraWeb3/Calc1.aspx.cs
@@ -0,0 +1,45 @@
+﻿using CalculadoraWeb3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculadoraWeb3
+{
+    public partial class Calc1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnMais_Click(object sender, EventArgs e)
+        {
+            //Calculo calc = new Calculo();
+            //calc.Valor1 = Convert.ToInt32(txtVal1.Text);
+            //calc.Valor2 = Convert.ToInt32(txtVal2.Text);
+
+            //calc.Resultado = calc.Valor1 + calc.Valor2;
+            //lblResult2.Text = calc.Resultado.ToString();
+
+            //ARMEZANANDO NA VIEWSTATE - VETOR
+            ViewState["valor1"] = txtVal1.Text;
+            ViewState["valor2"] = txtVal2.Text;
+
+            //RECUPERANDO VIEWSTATE
+            double result = Convert.ToDouble(ViewState["valor1"]) + Convert.ToDouble(ViewState["valor2"]);
+            lblResult2.Text = result.ToString();
+        }
+
+        protected void btnIgual_Click(object sender, EventArgs e)
+        {
+            //ENVIAR INFORMAÇÃO PARA OUTRA PAGINA - ARMAZENAMENTO NA SESSION
+            Session["valor1"] = txtVal1.Text;
+            Session["valor2"] = txtVal2.Text;
+
+            Response.Redirect("~/Calc2.aspx");
+        }
+    }
+}
---
+++ CalculadoraWeb3/CalculadoraWeb3/Models/Calculo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculadoraWeb3.Models
+{
+    public class Calculo
+    {
+
+        public int Valor1 { get; set; }
+        public int Valor2 { get; set; }
+        public int Resultado { get; set; }
+
+
+        public  int CalculoDosNumeros(int op)
+        {
+            int operacao = op;
+            switch (operacao)
+            {
+                case 1:
+                    Resultado = Valor1 + Valor2;
+                    break;
+                case 2:
+                    Resultado = Valor1 - Valor2;
+                    break;
+                case 3:
+                    Resultado = Valor1 * Valor2;
+                    break;
+                case 4:
+                    Resultado = Valor1 / Valor2;
+                    break;
+            }
+           
+            return Resultado;
+        }
+    }
+}
