eb66c607679203145dbf0b915243c5ee7f8594ef
1610832 <1610832@045LAB04005>
2017-05-23 22:23:48 -0300

    Entity Framework - Data Base First

---
+++ Aula2305/Aula2305/View/Base/BasePage.cs
@@ -0,0 +1,13 @@
+﻿using Aula2305.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aula2305.View
+{
+    public class BasePage : System.Web.UI.Page
+    {
+        LojaDBEntities contexto = new LojaDBEntities();
+    }
+}
---
+++ Aula2305/Aula2305/View/Produto/Lista.aspx.cs
@@ -0,0 +1,22 @@
+﻿using Aula2305.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aula2305.View.Produto
+{
+    public partial class Lista : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //CONTEXTO -- REFERENCIA PARA O BANCO DE DADOS
+            //LojaDBEntities contexto = new LojaDBEntities();
+            gvProdutos.DataSource = contexto.Produto.ToList();
+
+            gvProdutos.DataBind();
+        }
+    }
+}
