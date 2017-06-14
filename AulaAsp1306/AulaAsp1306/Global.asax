9ae832da5b53e8aaa3247117ba16d9a1c668ea5b
1610832 <1610832@045LAB04006>
2017-06-13 22:05:02 -0300

    View - Create - Annotation Model Inicio do MVC

---
+++ AulaAsp1306/AulaAsp1306/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AulaAsp1306.MvcApplication" Language="C#" %>
--- ProjetoWeb0106/ProjetoWeb0106/Models/Clube2DB.Designer.cs
+++ ProjetoWeb0106/ProjetoWeb0106/Models/Clube2DB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\1610832\Source\Repos\Aula1_Asp.Net2\ProjetoWeb0106\ProjetoWeb0106\Models\Clube2DB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\1610832\Documents\GitHub\ProjetoWeb0106\ProjetoWeb0106\Models\Clube2DB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
